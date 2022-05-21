ed58eb5c8143b38385d7f4dbca8e1bd1fcdf0902
adam-blakeney <adam.blakeney@outlook.com>
2022-05-21 13:43:50 +0000

    inital commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+intro_instructions = """ xxxxx """
+
+class GameArea:
+    """ this sets the area in which the player and computer will play.
+    """
+
+
+def player_hide():
+    """
+    this lets the player choose where they want to hide.
+    this will return an error if the input is not correct
+    """
