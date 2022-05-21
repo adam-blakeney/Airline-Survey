b0db505bce0ce9a163daeee208a38e46135b74c8
adam-blakeney <adam.blakeney@outlook.com>
2022-05-21 13:49:36 +0000

    added layout

--- run.py
+++ run.py
@@ -5,7 +5,22 @@ intro_instructions = """ xxxxx """
 class GameArea:
     """ this sets the area in which the player and computer will play.
     """
-
+    def __init__(self, name):
+        self.name = name
+        # Code credit on self.board layout # https://github.com/Damianjacob/MS3-Battleship-Game
+        self.board = [
+            [" ",  " A", "  B", "  C", "  D", "  E"],
+            ["1", "| |", "| |", "| |", "| |", "| |"],
+            ["2", "| |", "| |", "| |", "| |", "| |"],
+            ["3", "| |", "| |", "| |", "| |", "| |"],
+            ["4", "| |", "| |", "| |", "| |", "| |"],
+            ["5", "| |", "| |", "| |", "| |", "| |"],
+        ]
+        self.board_array = np.array(self.board)
+        self.computer_displayed_board = np.array(self.board)
+        self.user_score = 0
+        self.computer_score = 0
+        self.column_map = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
 
 def player_hide():
     """
